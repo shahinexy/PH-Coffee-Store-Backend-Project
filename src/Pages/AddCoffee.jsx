02084e916bff9ea4a65a8b141954b484790dd10c
shahinexy <shahinexy@gmail.com>
2024-04-22 15:29:53 +0600

    project ready for work

---
+++ src/Pages/AddCoffee.jsx
@@ -0,0 +1,10 @@
+
+const AddCoffee = () => {
+    return (
+        <div>
+            add coffee
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/Pages/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            update 
+        </div>
+    );
+};
+
+export default UpdateCoffee;
