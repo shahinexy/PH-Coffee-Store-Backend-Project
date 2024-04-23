af8a62b90007dc28793af8f33d52e93b4ed06626
shahinexy <shahinexy@gmail.com>
2024-04-23 12:20:35 +0600

    card update section added

--- src/Pages/UpdateCoffee.jsx
+++ src/Pages/UpdateCoffee.jsx
@@ -1,6 +1,21 @@
 import { Link } from "react-router-dom";
 
 const UpdateCoffee = () => {
+
+  const handleUpdateCoffee = e =>{
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const chef = form.chef.value;
+    const supplier = form.supplier.value;
+    const taste = form.taste.value;
+    const category = form.category.value;
+    const detail = form.detail.value;
+    const photo = form.photo.value;
+
+    const updateCoffee = {name, chef, supplier, taste, category, detail, photo}
+    console.log(updateCoffee);
+  }
   return (
     <div>
       <div className="text-center space-x-5">
@@ -11,7 +26,58 @@ const UpdateCoffee = () => {
           <button className="btn">Go to Add Coffee</button>
         </Link>
       </div>
-      update
+      
+      
+      <div className="max-w-6xl mx-auto p-10 bg-pink-100">
+      <div className="text-center my-8 space-y-4 ">
+          <h2 className="text-4xl font-bold">Update Existing Coffee Details</h2>
+          <p>
+            It is a long established fact that a reader will be distraceted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using Content here.
+          </p>
+        </div>
+      <form onSubmit={handleUpdateCoffee} className="space-y-8">
+          <div className="flex justify-evenly gap-10">
+            <div className="w-full ">
+              <p>Name</p>
+              <input className="w-full p-2" type="text" name="name" placeholder="Name" />
+            </div>
+            <div className="w-full ">
+              <p>Chef</p>
+              <input className="w-full p-2" type="text" name="chef" placeholder="Chef" />
+            </div>
+          </div>
+          <div className="flex justify-evenly gap-10">
+            <div className="w-full ">
+              <p>Supplier</p>
+              <input className="w-full p-2" type="text" name="supplier" placeholder="Supplier" />
+            </div>
+            <div className="w-full ">
+              <p>Taste</p>
+              <input className="w-full p-2" type="text" name="taste" placeholder="Taste" />
+            </div>
+          </div>
+          <div className="flex justify-evenly gap-10">
+            <div className="w-full ">
+              <p>Category</p>
+              <input className="w-full p-2" type="text" name="category" placeholder="Category" />
+            </div>
+            <div className="w-full ">
+              <p>Detail</p>
+              <input className="w-full p-2" type="text" name="detail" placeholder="Detail" />
+            </div>
+          </div>
+          <div className="w-full">
+            <p>Photo</p>
+            <input className="w-full p-2" type="text" name="photo" placeholder="Photo" />
+          </div>
+          <div>
+            <input className="w-full p-2 bg-purple-500 text-white font-semibold cursor-pointer" type="submit" value="Add Coffee" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Coffee.jsx
+++ src/components/Coffee.jsx
@@ -1,41 +1,60 @@
+import Swal from "sweetalert2";
 
+const Coffee = ({ data }) => {
+  const { _id, name, chef, photo, supplier } = data;
 
-const Coffee = ({data}) => {
-    const {_id, name, chef, photo, supplier} = data;
+  const danhleDelete = (id) => {
+    console.log(id);
 
-    const danhleDelete = id =>{
-        console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
         fetch(`http://localhost:5000/coffee/${id}`, {
-            method: "DELETE"
+          method: "DELETE",
         })
-        .then(res => res.json)
-        .then(data => {
+          .then((res) => res.json)
+          .then((data) => {
             console.log(data);
-            alert("delete success")
-        })
-    }
-    return (
-        <div>
-            <div className="p-6 border shadow-lg flex gap-4">
-                <div className="w-2/6">
-                    <img className="w-full h-full" src={photo} alt="" />
-                </div>
-                <div className="w-3/6 space-y-2">
-                    <p>Name: {name}</p>
-                    <p>Chef: {chef}</p>
-                    <p>Supplier: {supplier}</p>
-                    <p>Price: 20$</p>
-                </div>
-                <div>
-                    <button className="btn bg-slate-500">View</button>
-                    <br />
-                    <button className="btn bg-purple-500">Edit</button>
-                    <br />
-                    <button onClick={()=>danhleDelete(_id)} className="btn bg-red-300">Delete</button>
-                </div>
-            </div>
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          });
+      }
+    });
+  };
+  return (
+    <div>
+      <div className="p-6 border shadow-lg flex gap-4">
+        <div className="w-2/6">
+          <img className="w-full h-full" src={photo} alt="" />
         </div>
-    );
+        <div className="w-3/6 space-y-2">
+          <p>Name: {name}</p>
+          <p>Chef: {chef}</p>
+          <p>Supplier: {supplier}</p>
+          <p>Price: 20$</p>
+        </div>
+        <div>
+          <button className="btn bg-slate-500">View</button>
+          <br />
+          <button className="btn bg-purple-500">Edit</button>
+          <br />
+          <button onClick={() => danhleDelete(_id)} className="btn bg-red-300">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Coffee;
