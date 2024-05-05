2372482e00ada4837a31936775a8ab6385d3a383
shahinexy <shahinexy@gmail.com>
2024-05-05 11:59:43 +0600

    drak theme mode added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import { Link, useLoaderData } from "react-router-dom";
 import Coffee from "./components/Coffee";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
 function App() {
   const loaderData = useLoaderData();
   const [coffee, setCoffee] = useState(loaderData)
+
+  const [mode, setMode] = useState('ligth')
+
+
+  const html = document.documentElement;
+  const changeTheme = () => {
+
+      if (mode === "light") {
+          html.classList.remove("light")
+          html.classList.add("dark")
+          localStorage.setItem("mode", 'dark')
+          setMode('dark')
+      }
+      else {
+          html.classList.remove("dark")
+          html.classList.add("light")
+          localStorage.setItem("mode", 'light')
+          setMode('light')
+      }
+  }
+
+  useEffect(() => {
+      const currentMode = localStorage.getItem('mode') ;
+      console.log(currentMode);
+      html.classList.add(currentMode);
+      setMode(currentMode)
+  }, [ mode, html])
+
 
   return (
     <div className="max-w-7xl mx-auto my-3">
@@ -18,6 +45,7 @@ function App() {
         <Link to={"/register"}>
           <button className="btn">Register</button>
         </Link>
+        <button onClick={changeTheme} className="btn">Change theme</button>
       </div>
       <h1 className="text-4xl font-bold text-center my-8">Coffee House</h1>
       <div className="my-10 grid lg:grid-cols-3 md:grid-cols-2 gap-5">
---
+++ src/hooks/Theme.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+
+const Theme = () => {
+    const [mode, setMode] = useState('ligth')
+
+    const changeTheme = () => {
+        const html = document.documentElement;
+
+        if (mode === "light") {
+            html.classList.remove("light")
+            html.classList.add("dark")
+            localStorage.setItem("mode", 'dark')
+            setMode('dark')
+        }
+        else {
+            html.classList.remove("dark")
+            html.classList.add("light")
+            localStorage.setItem("mode", 'light')
+            setMode('light')
+        }
+    }
+
+    useEffect(() => {
+        const currentMode = localStorage.getItem('mode') ;
+        console.log(currentMode);
+        document.documentElement.classList.add(currentMode)
+        setMode(currentMode)
+    }, [])
+    return {changeTheme , mode}
+};
+
+export default Theme;
